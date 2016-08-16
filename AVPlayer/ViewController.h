980de16d1c502e48bd84540c0312e054b1c4c7bd
qiangxinyu <18735359200@163.com>
2016-08-16 15:09:45 +0800

    Initial Commit

---
+++ AVPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVPlayer
+//
+//  Created by 强新宇 on 16/8/16.
+//  Copyright © 2016年 强新宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
